bb4cbefca83268c277f569ac55046e1c73bb4f29
Arvent <timothy.tim.ng@gmail.com>
2015-04-01 14:09:38 -0400

    Added Edge Weighted Digraphs for Flight Maps

---
+++ src/util/DirectedEdge.java
@@ -0,0 +1,42 @@
+package util;
+
+
+public class DirectedEdge {
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight) {
+        if (v < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (w < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (Double.isNaN(weight)) {
+            throw new IllegalArgumentException();
+        }
+
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
+
+
+}
---
+++ src/util/EdgeWeightedDigraph.java
@@ -0,0 +1,62 @@
+package util;
+
+public class EdgeWeightedDigraph {
+    private final int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+
+    public EdgeWeightedDigraph(int V) {
+        if (V < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<DirectedEdge>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public void addEdge(DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        validateVertex(v);
+        validateVertex(w);
+        adj[v].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v) {
+        validateVertex(v);
+        return adj[v];
+    }
+
+    public int outdegree(int v) {
+        validateVertex(v);
+        return adj[v].size();
+    }
+
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> list = new Bag<DirectedEdge>();
+        for (int v = 0; v < V; v++) {
+            for (DirectedEdge e : adj(v)) {
+                list.add(e);
+            }
+        }
+        return list;
+    }
+}
