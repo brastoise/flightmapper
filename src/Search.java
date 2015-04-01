0a466031e659c523041ba50b9b1bb78bbde8f99a
Arvent <timothy.tim.ng@gmail.com>
2015-04-01 15:09:15 -0400

    BFS to search Digraph

--- src/Search.java
+++ src/Search.java
@@ -1,6 +1,38 @@
-import util.HashMap;
+import util.Digraph;
 public class Search {
-    private HashMap<Integer, Flight> data;
+    private boolean[] marked;
+    private int count;
 
+    public Search(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    public Search(Digraph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        for (int v : sources) {
+            if (!marked[v]) {
+                dfs(G, v);
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        count++;
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G,w);
+            }
+        }
+    }
+
+    public boolean marked(int v) {
+        return marked[v];
+    }
+
+    public int count() {
+        return count;
+    }
 
 }
