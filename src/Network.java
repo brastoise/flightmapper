eb8cfe1f97b583f9cd8d6a2c9010e24d6aea314b
Arvent <timothy.tim.ng@gmail.com>
2015-04-01 14:31:50 -0400

    Network can now parse list of Airports

--- src/Network.java
+++ src/Network.java
@@ -1,16 +1,30 @@
 import util.HashMap;
 import util.ReadCSV;
+import java.util.ArrayList;
 public class Network {
-    private ReadCSV input = new ReadCSV("data/StarAlliance.csv");
-    private HashMap<Integer, String> HM_idAirport = new HashMap<Integer, String>(100);
-    private HashMap<String, Integer> HM_AirportID = new HashMap<String, Integer>(100);
+    private static ReadCSV input = new ReadCSV("data/StarAlliance.csv");
+    private static HashMap<Integer, String> HM_idAirport = new HashMap<Integer, String>(100);
+    private static HashMap<String, Integer> HM_AirportID = new HashMap<String, Integer>(100);
 
     public static void init() {
-        HashMap<String, String> temp = ReadCSV.parse(0,1);
+        ArrayList<String> airports = input.readColumn(0);
+        ArrayList<String> seen = new ArrayList<String>();
+        int counter = 0;
+        for (String s : airports) {
+            if (!seen.contains(s)) {
+                HM_idAirport.put(counter, s);
+                HM_AirportID.put(s, counter);
+                seen.add(s);
+                counter++;
+            }
+        }
+    }
 
-
-
-
+    public static void main(String[] args) {
+        init();
+        for (String s : HM_AirportID.keys()) {
+            System.out.println(s);
+        }
     }
 
 }
