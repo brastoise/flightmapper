a9c6dac602fa0bdc29a38b51db43ce3ff3f10c84
arvent <timothy.tim.ng@gmail.com>
2015-04-08 21:35:15 -0400

    Documentation

--- src/util/Bag.java
+++ src/util/Bag.java
@@ -2,28 +2,57 @@ package util;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+/**
+ * 
+ * Bag data structure
+ * Based of the implementation in the Algorithms textbook
+ *
+ * @param <Item> - Data type of items to store inside bag
+ */
 public class Bag<Item> implements Iterable<Item> {
     private int N; // Number of items in bag
     private Node<Item> first; // First item in bag
 
+    
+    /**
+     * 
+     * Node helper class to store items
+     *
+     * @param <Item> - data type of items to store
+     */
     private static class Node<Item> {
         private Item item;
-        private Node<Item> next;
+        private Node<Item> next; // Pointer to next item in bag
     }
 
+    /**
+     * Initialises an empty bag
+     */
     public Bag() {
         first = null;
         N = 0;
     }
 
+    /**
+     * Check if the bag is empty
+     */
     public boolean isEmpty() {
         return first == null;
     }
 
+    
+    /**
+     * Returns the number of items in the bag
+     */
     public int size() {
         return N;
     }
 
+    /**
+     * Adds item into bag by making it the new first item
+     * similar to a stack
+     * @param item - Item to add into bag
+     */
     public void add(Item item) {
         Node<Item> oldFirst = first;
         first = new Node<Item>();
@@ -32,10 +61,18 @@ public class Bag<Item> implements Iterable<Item> {
         N++;
     }
 
+    /**
+     * Returns an iterator for the items in the bag
+     */
     public Iterator<Item> iterator() {
         return new ListIterator<Item>(first);
     }
 
+    /**
+     * Helper class to return an iterable version of items in bag
+     *
+     * @param <Item> - type of items stored
+     */
     private class ListIterator<Item> implements Iterator<Item> {
         private Node<Item> current;
 
--- src/util/HashMap.java
+++ src/util/HashMap.java
@@ -1,32 +1,58 @@
 package util;
 
-
+/**
+ * Linear probing hash table
+ * Implemented according to the specifications in the Algorithms textbook
+ *
+ * @param <Key> - data type for the key
+ * @param <Val> - data type for the value
+ */
 public class HashMap<Key,Val> {
     private int N; // Number of Key-Value pairs
     private int M; // Size of HashMap
-    private Key[] keys;
-    private Val[] vals;
+    private Key[] keys; // An array of keys
+    private Val[] vals; // An array of values
 
+    /**
+     * Initialises a HashMap with size M
+     * 
+     * @param M - initial size of HashMap
+     */
     public HashMap(int M) {
         this.M = M;
         keys = (Key[]) new Object[M];
         vals = (Val[]) new Object[M];
     }
 
-
-
+    /**
+     * Returns the size of the HashMap
+     * Note: Not number of key-value pairs, calculates the null values of HashMap as well
+     */
     public int size() {
         return M;
     }
 
+    /**
+     * Returns true if the HashMap has a size of 0
+     */
     public boolean isEmpty() {
         return size() == 0;
     }
 
+    /**
+     * Hashing Function
+     * 
+     * @param key - Key to be hashed
+     */
     public int hash(Key key) {
         return (key.hashCode() & 0x7fffffff) % M;
     }
 
+    /**
+     * Resizes the HashMap to the given size
+     * 
+     * @param newM - desired size of HashMap
+     */
     public void resize(int newM) {
         HashMap<Key, Val> temp = new HashMap<Key, Val>(newM);
         for (int i = 0; i < M; i++) {
@@ -39,6 +65,11 @@ public class HashMap<Key,Val> {
         M = temp.M;
     }
 
+    /**
+     * Given a key, find the matching value
+     * 
+     * @param key - Search for value associated with this key
+     */
     public Val get(Key key) {
         for (int i = hash(key); keys[i] != null; i = (i + 1) % M) {
             if (keys[i].equals(key)) {
@@ -48,6 +79,11 @@ public class HashMap<Key,Val> {
         return null;
     }
 
+    /**
+     * Find the key for a given value
+     * 
+     * @param val - value to search key for
+     */
     public Key getKey(Val val) {
         Iterable<Key> list = keys();
         for (Key k : list) {
@@ -58,14 +94,31 @@ public class HashMap<Key,Val> {
         return null;
     }
 
+    /**
+     * Checks the HashMap if a key-value pair exists
+     * 
+     * @param key
+     */
     public boolean containsKey(Key key) {
         return get(key) != null;
     }
 
+    /**
+     * Checks if key-value pair exists given value
+     * 
+     * @param val
+     */
     public boolean containsVal(Val val) {
         return getKey(val) != null;
     }
 
+    /**
+     * Inserts key-pair value into HashMap
+     * If value given is null, delete given key
+     * 
+     * @param key
+     * @param val
+     */
     public void put(Key key, Val val) {
         if (val == null) {
             delete(key);
@@ -88,6 +141,11 @@ public class HashMap<Key,Val> {
         N++;
     }
 
+    /**
+     * Deletes the key-value pair
+     * 
+     * @param key
+     */
     public void delete(Key key) {
         if (!containsKey(key)) {
             return;
@@ -121,6 +179,9 @@ public class HashMap<Key,Val> {
         assert check();
     }
 
+    /**
+     * Returns a list of keys in the HashMap
+     */
     public Iterable<Key> keys() {
         Queue<Key> queue = new Queue<Key>();
         for (int i = 0; i < M; i++) {
@@ -131,6 +192,9 @@ public class HashMap<Key,Val> {
         return queue;
     }
 
+    /**
+     * Returns a list of values in the HashMap
+     */
     public Iterable<Val> values() {
         Queue<Val> queue = new Queue<Val>();
         for (int i = 0; i < M; i++) {
@@ -141,6 +205,9 @@ public class HashMap<Key,Val> {
         return queue;
     }
 
+    /**
+     * Integrity check
+     */
     private boolean check() {
         if (M < 2*N) {
             System.err.println("Hash table size M = " + M + "; array size N = " + N);
@@ -157,12 +224,4 @@ public class HashMap<Key,Val> {
         }
         return true;
     }
-
-    public static void main(String[] args) {
-        HashMap<Integer, String> test = new HashMap<Integer, String>(10);
-        test.put(0, "a");
-        test.put(1, "b");
-        System.out.println(test.containsVal("z"));
-    }
-
 }
